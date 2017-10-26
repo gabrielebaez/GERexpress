59bbe6f3be41dc3624df90a170d261a7cd25e7fd
Gabriel Baez <gabriel@mctekk.com>
2017-10-26 15:00:30 -0700

    Basic Wrapper completed.

---
+++ app.js
@@ -0,0 +1,118 @@
+
+const express = require('express')
+const app = express()
+var bodyParser = require('body-parser');
+
+// configure app to get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var g = require('ger');
+var esm = new g.MemESM();
+var ger = new g.GER(esm);
+
+/////////////////////////////////////////////////////
+
+app.get('/', function(req, res){
+    res.send('hello recommender');
+})
+
+
+app.post('/create_namespace', function(req, res){
+    res.setHeader('Content-Type', 'application/json');
+    body = req.body;
+
+    ger.initialize_namespace(body.namespace)
+        .then((namespace) => {
+            res.send(JSON.stringify({ message: `namespace ${body.namespace} registrado.` }));
+        })
+        .catch((reason) => {
+            res.send(JSON.stringify({ message: 'error' }));
+        })
+})
+
+
+app.post('/destroy_namespace', function(req, res){
+    res.setHeader('Content-Type', 'application/json');
+    body = req.body;
+
+    ger.destroy_namespace(body.namespace)
+    .then((namespace) => {
+        res.send(JSON.stringify({ message: `namespace ${body.namespace} borrado.` }));
+    })
+    .catch((reason) => {
+        res.send(JSON.stringify({ message: 'error' }));
+    })
+})
+
+
+app.post('/add_events', function(req, res){
+    res.setHeader('Content-Type', 'application/json');
+    body = req.body;
+
+    ger.events(body.events)
+        .then((events) => {
+            res.send(JSON.stringify(events))
+        })
+        .catch((reason) => {
+            res.send(JSON.stringify({ message: 'error', reason: reason }));
+        })
+})
+
+
+app.get('/events', function(req, res){
+    res.setHeader('Content-Type', 'application/json');
+
+    ger.find_events('movies')
+        .then((events)=>{
+            res.send(JSON.stringify({ events: events }));
+        })
+        .catch((reason)=>{
+            res.send(JSON.stringify({ message: 'error', reason: reason }))
+        })
+})
+
+
+app.post('/recommendations', function(req, res){
+    res.setHeader('Content-Type', 'application/json');
+    body = req.body;
+
+    ger.recommendations_for_person(body.namespace, body.person, {actions: {likes: 1}})
+        .then(function(recommendations){
+            res.send(JSON.stringify(recommendations,null,2));
+        })
+})
+
+
+app.post('/recommendations_for_thing', function(req, res){
+    res.setHeader('Content-Type', 'application/json');
+    body = req.body;
+
+    ger.recommendations_for_thing(body.namespace, body.thing, body.configuration)
+        .then((response)=>{
+            res.send(JSON.stringify({recommendations: response}));
+        })
+        .catch((reason)=>{
+            res.send(JSON.stringify({message: reason}));
+        })
+})
+
+
+app.post('/thing_neighbourhood', function(req, res){
+    res.setHeader('Content-Type','application/json');
+    body = req.body;
+
+    ger.thing_neighbourhood(body.namespace, body.thing, body.actions, options = {})
+        .then((response)=>{
+            res.send(JSON.stringify({neighbourhood: response}));
+        })
+        .catch((reason)=>{
+            res.send(JSON.stringify({message: reason}))
+        })
+})
+
+/////////////////////////////////////////////////////
+
+app.listen(3000, function(){
+    console.log('example app listening on port 3000')
+})
